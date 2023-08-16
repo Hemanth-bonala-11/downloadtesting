a1e91bfff5226b1d35926566e27ea26e5ad39f1b
Hemanth-bonala-11 <128019707+Hemanth-bonala-11@users.noreply.github.com>
2023-08-16 16:05:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 /opt/render/project/src/.venv/bin/python -m pip install --upgrade pip
 pip install -r requirements.txt
-
-
 python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
